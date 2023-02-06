f0ed5df05e7b3a749f19098fac987ae238aa91de
Cujba Tiberiu Stefan <123594218+CujbaTiberiu@users.noreply.github.com>
2023-02-06 16:46:50 +0100

    day one js

---
+++ epicodeCorso/esercizi/week_2/day1/assets/js/primo.js
@@ -0,0 +1,4 @@
+alert('Benvenuto Prof - Primo alert!');
+alert('Secondo alert!😁')
+document.getElementById('titolo').innerHTML = 'Iniziamo la nostra pratica';
+document.write('output scritto da Javascript')
---
+++ epicodeCorso/esercizi/week_2/day1/assets/js/secondo.js
@@ -0,0 +1,11 @@
+var animalepantera = 'pantera';
+
+var animale_criceto = 'criceto';
+console.log(animale_criceto);
+
+var animaleGiraffa = 'giraffa';
+console.log(animaleGiraffa);
+
+var Animale_Leone = 'leone';
+console.log(Animale_Leone);
+
