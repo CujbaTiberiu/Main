33d2dbf6988fcddd19072435d5f5c11dca734abf
Cujba Tiberiu Stefan <123594218+CujbaTiberiu@users.noreply.github.com>
2023-02-08 17:16:49 +0100

    day3

---
+++ epicodeCorso/esercizi/week_2/day3/assets/js/D3.js
@@ -0,0 +1,176 @@
+/* ESERCIZIO 1
+ Scrivi un algoritmo per trovare il più grande tra due numeri interi.
+*/
+/* SCRIVI QUI LA TUA RISPOSTA */
+let numero1 = 2;
+let numero2 = 5;
+
+ 
+var maggiore = numero1 > numero2 ? numero1 : numero2;
+console.log('il numero piu grande è' + ' ' + maggiore);
+document.getElementById('esercizio1').innerHTML = 'il numero piu grande è' + ' ' + maggiore;
+
+/* ESERCIZIO 2
+  Scrivi un algoritmo che mostri "not equal" in console se un numero intero fornito è diverso da 5.
+*/
+/* SCRIVI QUI LA TUA RISPOSTA */
+let numero3 = 4;
+let numero4 = 5;
+
+var notEqual = (numero3 == numero4) ? 'equal' : 'not Equal';
+console.log(notEqual);
+document.getElementById('esercizio2').innerHTML = notEqual;
+
+
+
+/* ESERCIZIO 3
+  Scrivi un algoritmo che mostri "divisibile per 5" in console se un numero fornito è perfettamente divisibile per 5 (suggerimento: cerca l'operatore modulo su un motore di ricerca)
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+let numero5 = 20;
+
+var divisibile = (numero5 %5 == 0) ? 'divisibile per 5' : 'non divisibile per 5';
+console.log(divisibile);
+document.getElementById('esercizio3').innerHTML = divisibile;
+
+
+
+/* ESERCIZIO 4
+  Scrivi un algoritmo per verificare che, dati due numeri interi, il valore di uno di essi sia 8 oppure se la loro addizione/sottrazione sia uguale a 8.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+let numero6 = 8;
+let numero7 = 4;
+var otto = (numero6 == 8 || numero7 == 8 || numero6 + numero7 == 8 || numero6 - numero7 == 8 || numero7 - numero6 == 8 ) ? 'verificato' : 'non verificato';
+console.log(otto);
+document.getElementById('esercizio4').innerHTML = otto;
+
+/* ESERCIZIO 5
+  Stai lavorando su un sito di e-commerce. Stai salvando il saldo totale del carrello dell'utente in una variabile "totalShoppingCart".
+  C'è una promozione in corso: se il totale del carrello supera 50, l'utente ha diritto alla spedizione gratuita (altrimenti la spedizione ha un costo fisso pari a 10).
+  Crea un algoritmo che determini l'ammontare totale che deve essere addebitato all'utente per il checkout.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+var totalShoppingCart = 61;
+var spedizione = 10;
+var totalCost;
+
+if(totalShoppingCart > 50){
+  totalCost = totalShoppingCart;
+  document.getElementById('esercizio5').innerHTML = 'Hai diritto alla spedizione gratuita - Totale da pagare' + ' ' + totalCost;
+  console.log( 'Hai diritto alla spedizione gratuita - Totale da pagare' + ' ' + totalCost);
+} else {
+  totalCost = totalShoppingCart + spedizione;
+ document.getElementById('esercizio5').innerHTML = 'Non hai il diritto alla spedizione gratuita - Totale da pagare' + ' '+ totalCost;
+console.log('Non hai il diritto alla spedizione gratuita - Totale da pagare' + ' '+ totalCost);
+}
+
+
+
+/* ESERCIZIO 6
+  Stai lavorando su un sito di e-commerce. Oggi è il Black Friday e viene applicato il 20% su ogni prodotto.
+  Modifica la risposta precedente includendo questa nuova promozione nell'algoritmo, determinando come prima se le spedizioni sono gratuite oppure no e e calcolando il totale.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+var scontato =  totalShoppingCart - (totalShoppingCart * 0.2);
+
+if(scontato > 50){
+  totalCost = scontato;
+  document.getElementById('esercizio6').innerHTML = 'Hai diritto alla spedizione gratuita - Totale da pagare' + ' ' + totalCost;
+  console.log( 'Hai diritto alla spedizione gratuita - Totale da pagare' + ' ' + totalCost);
+} else {
+  totalCost = scontato + spedizione;
+  document.getElementById('esercizio6').innerHTML = 'Non hai il diritto alla spedizione gratuita - Totale da pagare' + ' '+ totalCost;
+  console.log('Non hai il diritto alla spedizione gratuita - Totale da pagare' + ' '+ totalCost);
+}
+
+
+/* ESERCIZIO 7
+  Crea tre variabili, e assegna un valore numerico a ciascuna di esse.
+  Utilizzando un blocco condizionale, crea un algoritmo per ordinarle secondo il loro valore, dal più alto al più basso.
+  Alla fine mostra il risultato in console.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+var n1 = 10;
+var n2 = 58;
+var n3 = 18;
+
+var riordinato = [n1, n2, n3];
+console.log(riordinato);
+document.getElementById('esercizio7').innerHTML = riordinato;
+
+
+riordinato.sort();
+console.log(riordinato);
+document.getElementById('esercizio7.1').innerHTML = riordinato;
+
+
+
+/* ESERCIZIO 8
+  Crea un algoritmo per verificare che un valore fornito sia un numero oppure no (suggerimento: cerca su un motore di ricerca "typeof").
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 9
+  Crea un algoritmo per controllare se un numero fornito sia pari o dispari (suggerimento: cerca l'operatore modulo su un motore di ricerca)
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 10
+  Modifica la logica del seguente algoritmo in modo che mostri in console il messaggio corretto in ogni circostanza.
+  let val = 7
+  if (val < 10) {
+      console.log("Meno di 10");
+    } else if (val < 5) {
+      console.log("Meno di 5");
+    } else {
+      console.log("Uguale a 10 o maggiore");
+    }
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 11
+  Fornito il seguente oggetto, scrivi del codice per aggiungere una proprietà "city", il cui valore sarà "Toronto".
+*/
+
+const me = {
+  name: 'John',
+  lastName: 'Doe',
+  skills: ['javascript', 'html', 'css'],
+}
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 12
+  Lavorando sempre sull'oggetto precedentemente fornito, scrivi del codice per rimuovere la proprietà "lastName".
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 13
+  Lavorando sempre sull'oggetto precedentemente fornito, scrivi del codice per rimuovere l'ultimo elemento della proprietà "skills".
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 14
+  Scrivi del codice per creare un array inizialmente vuoto. Riempilo successivamente con i numeri da 1 a 10.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 15
+  Scrivi del codice per sostituire l'ultimo elemento dell'array, ovvero il valore 10, con il valore 100.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
