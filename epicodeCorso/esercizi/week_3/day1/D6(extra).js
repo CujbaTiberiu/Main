65bf521bd4b25dad596b434c207a8c6a9647839c
Cujba Tiberiu Stefan <123594218+CujbaTiberiu@users.noreply.github.com>
2023-02-13 22:17:45 +0100

    js exercises

--- epicodeCorso/esercizi/week_3/day1/D6(extra).js
+++ epicodeCorso/esercizi/week_3/day1/D6(extra).js
@@ -8,6 +8,20 @@
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+const myArray2 = giveMeRandom(10);
+console.log(myArray2);
+//console.log(myArray2.filter(maggiori5))
+function boh() {
+    arr = myArray2.filter(maggiore5);
+    function maggiore5(x) {
+        return x > 5;
+    }
+    console.log("Values greater than 5:", arr);
+    const sum = arr.reduce((acc, curr) => acc + curr, 0);//acc = accumulated value curr = current value
+    console.log("Sum of values greater than 5:", sum);
+}
+
+boh();
 /* EXTRA 2
  Nel tuo eCommerce disponi di un'array di oggetti chiamato "shoppingCart". Ognuno di questi oggetti ha le seguenti proprietà: "price", "name", "id" e "quantity".
  Crea una funzione chiamata "shoppingCartTotal" che calcola il totale dovuto al negozio (tenendo conto delle quantità di ogni oggetto).
@@ -33,13 +47,13 @@ var shoppingCartTotal = function () {
     }
     return total;
 }
-console.log(shoppingCartTotal(shoppingCart));
+console.log(shoppingCartTotal());
 
 
 
 //function sumArrayElements(shoppingCart,1, index2) {
-   // const sum = arr[index1] + arr[index2];
-   // return sum;}
+// const sum = arr[index1] + arr[index2];
+// return sum;}
 
 /* EXTRA 3
  Nel tuo eCommerce disponi di un'array di oggetti chiamato "shoppingCart". Ognuno di questi oggetti ha le seguenti proprietà: "price", "name", "id" e "quantity".
@@ -48,13 +62,39 @@ console.log(shoppingCartTotal(shoppingCart));
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+const batteria = new Acquisto(20, 'Duracell', 'pila', 5);
+addToShoppingCart(batteria);
+
+function addToShoppingCart(article) {
+    shoppingCart.push(article);
+    console.log(`L'array ora contiene ${shoppingCart.length} elementi`);
+    console.log(shoppingCart);
+}
 /* EXTRA 4
  Nel tuo eCommerce disponi di un'array di oggetti chiamato "shoppingCart". Ognuno di questi oggetti ha le seguenti proprietà: "price", "name", "id" e "quantity".
  Crea una funzione chiamata "maxShoppingCart" che riceve l'array "shoppingCart" e ritorna l'oggetto più costoso in esso contenuto.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+function maxShoppingCart() {
+    var max = shoppingCart[0].price;
+    var myArticle = shoppingCart[0];
 
+    for (var i = 1; i < shoppingCart.length; i++) { //shoppingCart.map((element) => {
+        var element = shoppingCart[i];
+        if (element.price > max) {
+            max = element.price;
+            myArticle = element;
+        }
+    }
+
+    console.log(`L'articolo con il prezzo più alto è ${myArticle.name}, che costa Euro ${max}`);
+}
+/*
+maxShoppingCart();
+const maxItem = shoppingCart.reduce((acc, curr) => (curr.price > acc.price ? curr : acc), shoppingCart[0]);
+console.log(`L'articolo con il prezzo più alto è ${maxItem.name}, che costa Euro ${maxItem.price}`);
+*/
 /* EXTRA 5
  Nel tuo eCommerce disponi di un'array di oggetti chiamato "shoppingCart". Ognuno di questi oggetti ha le seguenti proprietà: "price", "name", "id" e "quantity".
  Crea una funzione chiamata "latestShoppingCart" che riceve l'array "shoppingCart" e ritorna l'ultimo elemento.
@@ -93,6 +133,19 @@ Crea una funzione chiamata "average" che riceve un array come parametro e ne rit
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+var miaData = new Date(2023, 01, 01);
+var miaData2 = new Date(2023, 0, 01);
+
+console.log(daysPassed(miaData));
+console.log(daysPassed(miaData2));
+
+function daysPassed(data) {
+    let oggi = new Date();
+    let diff = oggi - data;
+    diff = Math.floor(diff / 1000 / 60 / 60 / 24);
+    return `dal ${data} a ${oggi} sono passati ${diff} giorni`;
+}
+
 
 /* EXTRA 11
  Scrivi una funzione chiamata "matrixGenerator" che riceve come paremetri due numeri interi, "x" e "y".
