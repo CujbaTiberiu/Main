9d7b0eaf6bac0f7482e2b2ffec86b95e1c33fb92
Cujba Tiberiu Stefan <123594218+CujbaTiberiu@users.noreply.github.com>
2023-03-16 08:59:00 +0100

    daily

--- epicodeCorso/Units/Unit2/Week3/day3/assets/js/libreria.js
+++ epicodeCorso/Units/Unit2/Week3/day3/assets/js/libreria.js
@@ -6,8 +6,8 @@ fetch('https://striveschool-api.herokuapp.com/books')
         const container = document.querySelector('.container');
         const cartList = document.getElementById('myUl')
         const cartListTitle = document.getElementById('ulTitle');
-        const row = document.createElement('div');
 
+        const row = document.createElement('div');
         row.classList.add('row', 'row-cols-1', 'row-cols-sm-2', 'row-cols-md-3', 'row-cols-lg-4', 'my-3', 'justify-content-center');
 
         books.forEach(book => {
@@ -16,36 +16,40 @@ fetch('https://striveschool-api.herokuapp.com/books')
             col.classList.add('col', 'gy-3');
 
             const card = document.createElement('div');
-            card.classList.add('card', 'h-100'); // 'd-flex', ' flex-column'
+            card.classList.add('card', 'h-100'); // 'd-flex', 'flex-column'
 
             const cardImage = document.createElement('img');
-            cardImage.classList.add('card-img-top', 'img-fluid', 'h-100');
+            cardImage.classList.add('card-img-top');
             cardImage.src = book.img;
             cardImage.alt = book.title;
 
             const cardBody = document.createElement('div');
-            cardBody.classList.add('card-body'); // ,'flex-grow-1'
+            cardBody.classList.add('card-body', 'position-relative'); // ,'flex-grow-1'
 
             const cardTitle = document.createElement('h5');
             cardTitle.classList.add('card-title');
             cardTitle.textContent = book.title;
 
             const cardPrice = document.createElement('p');
-            cardPrice.classList.add('card-text');
-            cardPrice.textContent = `$${book.price}`;
+            cardPrice.classList.add('card-text', 'mb-5');
+            cardPrice.textContent = `${book.price}$`;
+
+            const btnDiv = document.createElement('div');
+            btnDiv.classList.add('d-flex', 'justify-content-around', 'position-absolute', 'bottom-0', 'ms-5', 'mb-1')
 
             const btnScarta = document.createElement('a');
-            btnScarta.classList.add('btn', 'btn-outline-danger');
+            btnScarta.classList.add('btn', 'btn-outline-danger', 'p-1');
             btnScarta.innerHTML = 'Discard';
 
             const btnAdd = document.createElement('a');
-            btnAdd.classList.add('btn', 'btn-outline-success', 'mx-3');
+            btnAdd.classList.add('btn', 'btn-outline-success', 'mx-3', 'p-1');
             btnAdd.innerHTML = 'Add to Cart';
 
             cardBody.appendChild(cardTitle);
             cardBody.appendChild(cardPrice);
-            cardBody.appendChild(btnScarta);
-            cardBody.appendChild(btnAdd);
+            cardBody.appendChild(btnDiv)
+            btnDiv.appendChild(btnScarta);
+            btnDiv.appendChild(btnAdd);
             card.appendChild(cardImage);
             card.appendChild(cardBody);
             col.appendChild(card);
@@ -57,16 +61,21 @@ fetch('https://striveschool-api.herokuapp.com/books')
 
             btnAdd.addEventListener('click', function () {
                 cartListTitle.classList.remove('d-none');
+
                 const newLi = document.createElement('li');
                 newLi.classList.add('text-white', 'my-2', 'mx-5')
                 newLi.innerText = `Title: ${book.title} | Price: ${book.price}$`;
+
                 const btnDiscard = document.createElement('a');
                 btnDiscard.classList.add('btn', 'btn-outline-danger', 'mx-2');
                 btnDiscard.innerHTML = 'Discard';
+
                 newLi.appendChild(btnDiscard);
                 cartList.appendChild(newLi);
+
                 btnDiscard.addEventListener('click', function () {
                     cartList.removeChild(newLi);
+                    //cartListTitle.classList.add('d-none');
                 })
             })
         })
